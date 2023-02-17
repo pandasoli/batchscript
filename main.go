17f0201e0d79d3071d201cbe674e132c9f83e9cc
pandasoli <sadpandasoli@gmail.com>
2023-02-17 20:51:21 -0300

    feat: add REPL

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+  "batchscript/lexer"
+  "batchscript/token"
+
+  "bufio"
+  "fmt"
+  "os"
+)
+
+
+func main() {
+  scanner := bufio.NewScanner(os.Stdin)
+
+  for {
+    fmt.Printf("> ")
+
+    scanned := scanner.Scan()
+    if !scanned { break }
+    line := scanner.Text()
+    if line == "" { break }
+
+    lex := lexer.New(line)
+
+    for {
+      tok := lex.Lex()
+      fmt.Printf("%+v\n", tok)
+
+      if tok.Kind == token.EOF {
+        break
+      }
+    }
+  }
+}
